fb355f2d52319a988dd5d8141d0d171605e200bf
aijeykob <aijeykob@gmail.com>
2019-12-23 15:13:59 +0200

    add try catch & refactor

--- src/Controller/HeroController.php
+++ src/Controller/HeroController.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace App\Controller;
+use mysql_xdevapi\Exception;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Entity\Hero;
@@ -19,26 +20,32 @@ class HeroController extends AbstractController
 
     public function getHero(Request $request): Response
     {
-        $heroRepository = $this->getDoctrine()->getRepository(Hero::class);
-        $items = $heroRepository->findAll();
-        $arrayHeroes  = array();
-        foreach ($items  as $hero) {
-            $arrayHeroes[] = array(
-                'nickname' => $hero->getNickname(),
-                'real_name' => $hero->getRealName(),
-                'origin_description' => $hero->getOriginDescription(),
-                'superpowers' => $hero->getSuperpowers(),
-                'catch_phrase' => $hero->getCatchPhrase(),
-                'fileName' => $hero->getHeroImage(),
-                'id' => $hero->getId()
-            );
-        }
         $response = new JsonResponse();
-        $response->setContent(json_encode($arrayHeroes));
         $response->headers->set('Content-Type', 'application/json');
         $response->headers->set('Access-Control-Allow-Origin', '*');
-        return $response;
+        try {
+            $heroRepository = $this->getDoctrine()->getRepository(Hero::class);
+            $items = $heroRepository->findAll();
+            $arrayHeroes  = array();
+            foreach ($items  as $hero) {
+                $arrayHeroes[] = array(
+                    'nickname' => $hero->getNickname(),
+                    'real_name' => $hero->getRealName(),
+                    'origin_description' => $hero->getOriginDescription(),
+                    'superpowers' => $hero->getSuperpowers(),
+                    'catch_phrase' => $hero->getCatchPhrase(),
+                    'fileName' => $hero->getHeroImage(),
+                    'id' => $hero->getId()
+                );
+            }
 
+            $response->setContent(json_encode($arrayHeroes));
+            return $response;
+        }
+        catch (Exception $e){
+            $response->setContent($e);
+            return $response;
+        }
     }
 
 
@@ -49,38 +56,29 @@ class HeroController extends AbstractController
 
     public function createHero(Request $request): Response
     {
-        $entityManager = $this->getDoctrine()->getManager();
-        $hero = new Hero();
-        $hero->setNickname($request->request->get('nickname'));
-        $hero->setRealName($request->request->get('real_name'));
-        $hero->setOriginDescription($request->request->get('origin_description'));
-        $hero->setSuperpowers($request->request->get('superpowers'));
-        $hero->setCatchPhrase($request->request->get('catch_phrase'));
-        $hero->setHeroImage('fileName');
-        $entityManager->persist($hero);
-        $entityManager->flush();
-
-        $heroRepository = $this->getDoctrine()->getRepository(Hero::class);
-        $allHeroes = $heroRepository->findAll();
-        $arrayHeroes  = array();
-        foreach ($allHeroes  as $hero) {
-            $arrayHeroes[] = array(
-                'nickname' => $hero->getNickname(),
-                'real_name' => $hero->getRealName(),
-                'origin_description' => $hero->getOriginDescription(),
-                'superpowers' => $hero->getSuperpowers(),
-                'catch_phrase' => $hero->getCatchPhrase(),
-                'fileName' => $hero->getHeroImage(),
-
-            );
-        }
         $response = new JsonResponse();
-        $response->setContent('success');
         $response->headers->set('Content-Type', 'application/json');
         $response->headers->set('Access-Control-Allow-Origin', '*');
-        return $response;
 
+        try{
+            $entityManager = $this->getDoctrine()->getManager();
+            $hero = new Hero();
+            $hero->setNickname($request->request->get('nickname'));
+            $hero->setRealName($request->request->get('real_name'));
+            $hero->setOriginDescription($request->request->get('origin_description'));
+            $hero->setSuperpowers($request->request->get('superpowers'));
+            $hero->setCatchPhrase($request->request->get('catch_phrase'));
+            $hero->setHeroImage('fileName');
+            $entityManager->persist($hero);
+            $entityManager->flush();
+            $response->setContent('success');
+            return $response;
+        }
 
+        catch (Exception $e) {
+            $response->setContent($e);
+            return $response;
+        }
     }
 
     /**
@@ -90,22 +88,28 @@ class HeroController extends AbstractController
 
     public function updateHero(Request $request): Response
     {
-        $entityManager = $this->getDoctrine()->getManager();
-        $id = $request->request->get('id');
-        $heroRepository = $this->getDoctrine()->getRepository(Hero::class);
-        $hero = $heroRepository->find($id);
-        $hero->setNickname($request->request->get('nickname'));
-        $hero->setRealName($request->request->get('real_name'));
-        $hero->setOriginDescription($request->request->get('origin_description'));
-        $hero->setSuperpowers($request->request->get('superpowers'));
-        $hero->setCatchPhrase($request->request->get('catch_phrase'));
-        $hero->setHeroImage('fileName');
-        $entityManager->flush();
         $response = new JsonResponse();
-        $response->setContent('success');
         $response->headers->set('Content-Type', 'application/json');
         $response->headers->set('Access-Control-Allow-Origin', '*');
-        return $response;
+        try{
+            $entityManager = $this->getDoctrine()->getManager();
+            $id = $request->request->get('id');
+            $heroRepository = $this->getDoctrine()->getRepository(Hero::class);
+            $hero = $heroRepository->find($id);
+            $hero->setNickname($request->request->get('nickname'));
+            $hero->setRealName($request->request->get('real_name'));
+            $hero->setOriginDescription($request->request->get('origin_description'));
+            $hero->setSuperpowers($request->request->get('superpowers'));
+            $hero->setCatchPhrase($request->request->get('catch_phrase'));
+            $hero->setHeroImage('fileName');
+            $entityManager->flush();
+            $response->setContent('success');
+            return $response;
+        }
+        catch (Exception $e) {
+            $response->setContent($e);
+            return $response;
+        }
 
     }
 
@@ -116,16 +120,23 @@ class HeroController extends AbstractController
 
     public function deleteHero(Request $request): Response
     {
-        $id = $request->query->get('id');
-        $heroRepository = $this->getDoctrine()->getRepository(Hero::class);
-        $hero = $heroRepository->find($id);
-        $entityManager = $this->getDoctrine()->getManager();
-        $entityManager->remove($hero);
-        $entityManager->flush();
         $response = new JsonResponse();
-        $response->setContent('success');
         $response->headers->set('Content-Type', 'application/json');
         $response->headers->set('Access-Control-Allow-Origin', '*');
-        return $response;
+        try{
+            $id = $request->query->get('id');
+            $heroRepository = $this->getDoctrine()->getRepository(Hero::class);
+            $hero = $heroRepository->find($id);
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->remove($hero);
+            $entityManager->flush();
+            $response->setContent('success');
+            return $response;
+        }
+        catch (Exception $e) {
+            $response->setContent($e);
+            return $response;
+        }
+
     }
 }
--- src/Repository/HeroRepository.php
+++ src/Repository/HeroRepository.php
@@ -19,32 +19,4 @@ class HeroRepository extends ServiceEntityRepository
         parent::__construct($registry, Hero::class);
     }
 
-    // /**
-    //  * @return Hero[] Returns an array of Hero objects
-    //  */
-    /*
-    public function findByExampleField($value)
-    {
-        return $this->createQueryBuilder('h')
-            ->andWhere('h.exampleField = :val')
-            ->setParameter('val', $value)
-            ->orderBy('h.id', 'ASC')
-            ->setMaxResults(10)
-            ->getQuery()
-            ->getResult()
-        ;
-    }
-    */
-
-    /*
-    public function findOneBySomeField($value): ?Hero
-    {
-        return $this->createQueryBuilder('h')
-            ->andWhere('h.exampleField = :val')
-            ->setParameter('val', $value)
-            ->getQuery()
-            ->getOneOrNullResult()
-        ;
-    }
-    */
 }
